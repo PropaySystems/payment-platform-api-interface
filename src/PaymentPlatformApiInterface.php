9a64c7f1a29a39e310915c869de12893f5ce52d3
Marius Odendaal <marius@datakrag.co.za>
2024-07-19 15:51:07 +0200

    Add Filters

--- src/PaymentPlatformApiInterface.php
+++ src/PaymentPlatformApiInterface.php
@@ -22,15 +22,27 @@ class PaymentPlatformApiInterface
      * @var mixed|string
      */
     private mixed $requestType;
+    /**
+     * @var array|mixed
+     */
+    private mixed $data;
 
-    public static function contacts(string $token, array $data): self
+    public static function contacts(string $token, array $filters = [], array $includes = []): self
     {
+        $data = [
+            'query' => http_build_query(['filter' => $filters, 'include' => $includes])
+        ];
+
         return new static($token, 'contacts', 'GET', $data);
     }
 
-    public static function contact(string $token, $key, array $data): self
+    public static function contact(string $token, $id, array $includes): self
     {
-        return new static($token, 'contacts/show/'.$key, 'GET', $data);
+        $data = [
+            'query' => http_build_query(['include' => $includes])
+        ];
+
+        return new static($token, 'contacts/show/' . $id, 'GET', $data);
     }
 
     public static function createContact(string $token, array $data): self
@@ -40,18 +52,19 @@ class PaymentPlatformApiInterface
 
     public static function updateContact(string $token, $key, array $data): self
     {
-        return new static($token, 'contacts/'.$key, 'PUT', $data);
+        return new static($token, 'contacts/' . $key, 'PUT', $data);
     }
 
     public function __construct(protected $token, string $endpoint, $requestType = 'GET', $data = [],)
     {
         $this->client = new Client(['base_uri' => self::$baseUrl . '/' . self::$version . '/']);
         $this->headers = [
-            'Authorization' => 'Bearer ' . base64_encode($token),
+            'Authorization' => 'Bearer ' . $token,
             'Accept' => 'application/json'
         ];
         $this->endpoint = $endpoint;
         $this->requestType = $requestType;
+        $this->data = $data;
     }
 
     /**
@@ -59,7 +72,8 @@ class PaymentPlatformApiInterface
      */
     public function get(): string
     {
-        return $this->client->request($this->requestType, $this->endpoint, ['headers' => $this->headers])->getBody();
+        $data = array_merge($this->data, ['headers' => $this->headers]);
+        return $this->client->request($this->requestType, $this->endpoint, $data)->getBody();
     }
 
 
